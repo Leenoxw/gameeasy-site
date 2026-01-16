6d89641ac78478209089ff02d92bdde1296e0e0c
Fatih <emiralfatih19@gmail.com>
2026-01-16 23:40:20 +0100

    Récupération complète des fichiers

--- gameeasy-site/astro.config.mjs
+++ gameeasy-site/astro.config.mjs
@@ -1,5 +1,4 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Leenoxw.github.io',
+  base: '/gameeasy-site', // Très important pour charger le CSS en ligne
+});
