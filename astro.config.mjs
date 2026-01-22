5d5b6cd1bfa300c14ef7d4f9d57bac7ed5512e77
Fatih <emiralfatih19@gmail.com>
2026-01-22 11:52:52 +0100

    Force build: Fix : jeu cracks

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // Remplace l'ancienne adresse par ton nouveau domaine
-  site: 'https://www.gameeasy.fr/',
-  base: '/', 
-});
