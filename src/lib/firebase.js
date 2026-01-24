0cfbb196cd70f76852a7ee14d674e89b94f77465
Fatih <emiralfatih19@gmail.com>
2026-01-24 18:32:26 +0100

    Force build: Fix : page inscription et fonctionnalité

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = { 
-    apiKey: "...", 
-    authDomain: "...", 
-    projectId: "...", 
-    storageBucket: "...", 
-    messagingSenderId: "...", 
-    appId: "..." 
+  apiKey: "AIzaSyAEso1EIVI1f4EmdQSZ3z5PvWlpoP7mwYE",
+  authDomain: "role-de-l-utilisateur.firebaseapp.com",
+  projectId: "role-de-l-utilisateur",
+  storageBucket: "role-de-l-utilisateur.firebasestorage.app",
+  messagingSenderId: "1043852188463",
+  appId: "1:1043852188463:web:ac7038084ec18e05dc9fda",
+  measurementId: "G-Q1GN8GCZR9"
 };
 
 const app = initializeApp(firebaseConfig);
