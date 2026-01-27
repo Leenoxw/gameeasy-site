5bd540879d84f0b5d22e3ea4cfd30c941b99e0fa
Fatih <emiralfatih19@gmail.com>
2026-01-27 12:42:05 +0100

    Force build: Fix : page cracks

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Remplace l'ancienne adresse par ton nouveau domaine
+  site: 'https://www.gameeasy.fr/',
+  base: '/', 
+});
